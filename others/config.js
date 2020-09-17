ceff95f7d5184bae5854a6be41950056b18a27ac
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-09-17 12:45:05 +0530

    Config File

---
+++ others/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  github: 'https://github.com/Scientific-Guy',
+  discord: 'https://discord.gg/FrduEZd',
+  npm: 'https://www.npmjs.com/~science_spot_codes',
+  logo: 'https://avatars1.githubusercontent.com/u/69678617?s=460&u=a3620f50c7ca8d7cf01d88eb05185cf27ed28be1&v=4',
+}
